532b2bc8dc6b25df738447e7652b0b63b83b4226
John Patrick Trinidad <Ptrinidad765@gmail.com>
2025-02-16 17:46:59 +0800

    updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
+// import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 // import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 // import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
   plugins: [
     react(),  
-    rollupNodePolyFill()
+    // rollupNodePolyFill()
   ],
   define: {
     global: 'globalThis' // polyfill global variable
